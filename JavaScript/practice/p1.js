ab59f2b5f5afa4af7c781d6b322568a7d1f60f98
gangagamit <gangagamit350@gmail.com>
2024-01-28 17:25:58 +0530

    28-1-2024

--- JavaScript/practice/p1.js
+++ JavaScript/practice/p1.js
@@ -20,33 +20,12 @@ let a = 50;
     document.write("<b>a is larger number</b>");
   }
   else
-  {
+  {a
     document.write("<b>b is larger number</b>");
   }
 }
 */
-/*3) Perform arithmatic operator
 
-{
-    let a = 2;
-    let b = 3;
-    let c = a + b;
-
- document.write(c+"<br>");
-
-    switch(c)
-    {
-        case 1 : document.write("total");
-        break;
-        case 2 : document.write("print a");
-        break;
-        default:
-            document.write("wrong input");
-
-    }
-
-}
-*/
 // 4)
 // {
 //     let maths = 85;
@@ -90,46 +69,111 @@ let a = 50;
 // }
 // 5)
 
-{
-    let a = 3;
-    let b = 2;
-    let c = 6;
+// {
+//     let a = 3;
+//     let b = 2;
+//     let c = 6;
 
-    if(a > b && a < c)
+//     if(a > b && a < c)
+//     {
+//         if(a > b)
+//         {
+//             document.write(b);
+//         }
+//         else{
+//             document.write(a);
+//         }
+
+//     }
+//     else if(a < c && b > c)
+//         {
+
+//             if(a < c)
+//             {
+//                 document.write(a);
+//             }
+        
+//         else
+//             {
+//             document.write(c);
+//             }
+//         }
+    
+//         else if(c > b && c < a)
+
+//         {
+//             if(c > b)
+//             {
+//                 document.write(c);
+//             }
+//             else{
+//                 document.write(a);
+//             }
+//         }
+
+// }
+// 6)
+
+{
+    let input = 10;
+    switch(input)
     {
-        if(a > b)
-        {
-            document.write(b);
-        }
-        else{
-            document.write(a);
-        }
+
+        case 1 :
+            let a = 2;
+            let b = 3;
+            let c = a + b;
+            document.write(c);
+            break;
+
+        case 2 :
+            let A = 2;
+            let B = 3;
+            let C = A - B;
+            document.write(C);
+            break;
+        case 3 :
+            let x = 2;
+            let y = 3;
+            let z = x / y;
+            document.write(z);
+            break;
+        
+        case 4 :
+            let X = 2;
+            let Y = 3;
+            let Z = X % Y;
+            document.write(Z);
+            break;
+        case 5 :
+            let g = 2;
+            ++g;
+            document.write(g);
+            break;
+        case 6 :
+            let h = 3;
+            h++;
+            document.write(h);
+            break;
+        case 7 :
+            let i = 8;
+            --i;
+            document.write(i);
+            break;
+        case 8 :
+            let j = 9;
+            j--;
+            document.write(j);
+            break;
+        case 9 :
+            let k = 4;
+            let l = 3;
+            let m = k ** l;
+            document.write(m);
+            break;
+        default :
+            document.write("wrong input");
 
     }
-    else if(a < c && b > c)
-        {
-
-            if(a < c)
-            {
-                document.write(a);
-            }
-        
-        else
-            {
-            document.write(c);
-            }
-        }
-    
-        else if(c > b && c < a)
-
-        {
-            if(c > b)
-            {
-                document.write(c);
-            }
-            else{
-                document.write(a);
-            }
-        }
-
 }
+
