87d14172c53d98c8d4934f559446ca9a90961af9
gangagamit <gangagamit350@gmail.com>
2024-01-29 16:00:08 +0530

    29-1-2024

---
+++ JavaScript/lecture5.js
@@ -0,0 +1,15 @@
+// {
+//     window.alert("this is javascript");
+//     alert("Hello user");
+// }
+
+// {
+//     let i = -5;
+//     alert(i ? "nagative numver" : "positive number");
+// }
+{
+    const message = prompt();
+    document.write(message);
+    let msg = confirm("this is javascript");
+    document.write(msg);
+}
