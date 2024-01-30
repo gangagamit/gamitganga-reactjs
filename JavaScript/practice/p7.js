d71b685654b74b111d0643ba16095974d27579b0
gangagamit <gangagamit350@gmail.com>
2024-01-30 18:12:17 +0530

    30-1-2024

---
+++ JavaScript/practice/p7.js
@@ -0,0 +1,63 @@
+
+// break and Continue statement
+ {
+//     let i;
+    
+//         for(i = 0; i<=6; i++)
+//         {
+//             document.write(i + "<br>");
+//             if(i==6)
+//             {
+
+//                 i++;
+//                 break;
+//             }
+//         }
+    
+}
+{
+    // let i = 4;
+    // while(i<=10)
+    // {
+    //     if(i%2!=0)
+    //     {
+    //         console.log(i);
+    //         break;
+    //     }
+    //     i++;
+    // }
+}
+
+// loop with a conditional break statement based on user input
+{
+    // let i = prompt();
+    // while(i<=6)
+    // {
+    //     if(i==5)
+    //     {
+    //         document.write("same number");
+    //         break;
+    //     }
+    //     else{
+    //         document.write("Not a same number");
+    //     }
+    //     i++;
+    // }
+
+}
+//Continue 
+{
+    let i;
+    for(i=1; i<=10; i++)
+    {
+        if(i%2!==0)
+        {
+            continue;
+        
+        }
+        document.write(i);
+    }
+}
+{
+    
+}
