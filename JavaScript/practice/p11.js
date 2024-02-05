217afa539278aa3dfc9df2cb3cf06fdc192fcea6
gangagamit <gangagamit350@gmail.com>
2024-02-05 19:44:26 +0530

    5-2-2024

---
+++ JavaScript/practice/p11.js
@@ -0,0 +1,14 @@
+{
+    const magicObject = {
+        val:1,
+        get a()
+        {
+            return this.val++;
+        }
+    };
+    console.log(magicObject.a);
+    console.log(magicObject.a);
+    console.log(magicObject.a);
+    console.log(magicObject.a);
+    console.log(magicObject.a);
+}
