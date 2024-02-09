f371d9c4c84ee41d6fa1343517d63e1567beb12f
gangagamit <gangagamit350@gmail.com>
2024-02-09 19:17:27 +0530

    9-2-2024

---
+++ JavaScript/for-while-js/s-9.js
@@ -0,0 +1,28 @@
+//for loop
+{
+    // let i=1;
+    // let n= prompt("enter a value");
+    // for(i=n; i>0;i--)
+    // {
+    //     console.log(i);
+    // }
+}
+//while loop
+{
+    // let i = n;
+    // let n = 10;
+    // while(i>0)
+    // {
+    //     console.log(i);
+    //     i--;
+    // }
+}
+//do while
+{
+    // let i = 1;
+    // let n = prompt("enter a value")
+    // do{
+    //     console.log(i);
+    //     i--;
+    // }while(i<=n);
+}
